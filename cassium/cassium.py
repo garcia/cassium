5586046cd5616f4f385b76d337b62b68ebcf57da
Grant Garcia <github@grantgarcia.org>
2012-10-12 16:36:47 -0700

    Fixed plugin imports

--- cassium/cassium.py
+++ cassium/cassium.py
@@ -33,20 +33,34 @@ class Cassium(SingleServerIRCBot):
     plugins = []
 
     def __init__(self, config=None):
+        global cfg
         # config must be given if the import above failed
         if config: cfg = config
-        elif not cfg: raise AttributeError("no configuration found")
+        elif not cfg: raise AttributeError('no configuration found')
         # initialize the IRC bot
         SingleServerIRCBot.__init__(self, [(cfg.server, cfg.port)], cfg.nick,
             cfg.realname)
         # import plugins
-        for plugin in cfg.autoload:
-            self.load_plugin(plugin)
+        self.plugins = self.load_plugins_recursively('plugins/')
+    
+    def load_plugins_recursively(self, directory):
+        plugins = []
+        for node in glob.iglob(directory + '*'):
+            if os.path.isdir(node):
+                plugins.extend(self.load_plugins_recursively(node + '/'))
+            elif node.endswith('.py') and '__init__' not in node:
+                # Convert filesystem path to dot-delimited path
+                path = os.path.splitext(node)[0].replace(os.path.sep, '.')
+                plugins.append(self.load_plugin(path))
+        return plugins
 
     def load_plugin(self, plugin):
         """Loads or reloads a plugin."""
         # Import the plugin
-        this_plugin = __import__('plugins.' + plugin)
+        this_plugin = __import__(plugin)
+        for component in plugin.split('.')[1:]:
+            this_plugin = getattr(this_plugin, component)
+
         # Search for an existing copy of the plugin
         for i, plugin in enumerate(self.plugins):
             if this_plugin.__name__ == plugin.__name__:
