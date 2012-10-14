3b434f53aaefab531cb0d303995d7553606e332d
Grant Garcia <github@grantgarcia.org>
2012-10-14 00:46:21 -0700

    Added plugin _core.control

---
+++ plugins/_core/control.py
@@ -0,0 +1,20 @@
+from cassium.plugin import CassiumPlugin
+
+class Control(CassiumPlugin):
+
+    triggers = [
+        '^`sudo (join|leave|nick|import)',
+    ]
+
+    def run(self, query, response):
+        if query.nick not in query.config.admins:
+            return response.msg('You are not permitted to use this command.')
+        if query.words[1] == 'import':
+            response.load('plugins.' + query.words[2])
+            response.msg('Loaded ' + query.words[2] + '.')
+        elif query.words[1] == 'join':
+            response.join(query.words[2])
+        elif query.words[1] == 'leave':
+            response.leave(query.words[2])
+        elif query.words[1] == 'nick':
+            response.nick(query.words[2])
