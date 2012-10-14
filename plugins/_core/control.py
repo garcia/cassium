08e512fbf937414f7b6759e7478b74961c122c2a
Grant Garcia <github@grantgarcia.org>
2012-10-14 13:55:52 -0700

    Changed triggers for _core.control

--- plugins/_core/control.py
+++ plugins/_core/control.py
@@ -3,18 +3,18 @@ from cassium.plugin import Plugin
 class Control(Plugin):
 
     triggers = [
-        '^`sudo (join|leave|nick|import)',
+        '^`(join|leave|nick|import)',
     ]
 
     def run(self, query, response):
         if query.nick not in query.config.admins:
             return response.msg('You are not permitted to use this command.')
-        if query.words[1] == 'import':
+        if query.words[0] == '`join':
+            response.join(query.words[2])
+        elif query.words[0] == '`leave':
+            response.leave(query.words[2])
+        elif query.words[0] == '`nick':
+            response.nick(query.words[2])
+        elif query.words[0] == '`import':
             response.load('plugins.' + query.words[2])
             response.msg('Loaded ' + query.words[2] + '.')
-        elif query.words[1] == 'join':
-            response.join(query.words[2])
-        elif query.words[1] == 'leave':
-            response.leave(query.words[2])
-        elif query.words[1] == 'nick':
-            response.nick(query.words[2])
