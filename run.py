da00c38e2e3fd643abc7517be9f54a8a3fe53cca
Grant Garcia <github@grantgarcia.org>
2012-10-15 19:07:01 -0700

    Cleanup

--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
+import os
+import sys
+
 from twisted.internet import reactor
 
 from cassium.cassium import Cassium, CassiumFactory
-#import config
 
 def main():
     factory = CassiumFactory()
-    #reactor.connectTCP(config.server, config.port, factory)
     reactor.connectTCP('localhost', 6667, factory)
     reactor.run()
-
 
 if __name__ == "__main__":
     main()
